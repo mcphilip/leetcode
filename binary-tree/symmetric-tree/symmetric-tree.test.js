4e12d03724b62bb94529026938424734240d7452
mcphilip <mcclellandphilip@yahoo.com>
2020-12-20 05:12:12 -0600

    adding recursive is symmetric test cases

--- binary-tree/symmetric-tree/symmetric-tree.test.js
+++ binary-tree/symmetric-tree/symmetric-tree.test.js
@@ -1,4 +1,5 @@
 const { setupTree, isSymmetric: isSymmetricIterative } = require('./symmetric-tree-iterative');
+const { isSymmetric: isSymmetricRecursive } = require('./symmetric-tree-recursive');
 
 describe('isSymmetric (iterative)', () => {
     it('should return false when tree is null', () => {
@@ -20,5 +21,28 @@ describe('isSymmetric (iterative)', () => {
         const tree = setupTree([1,2,2,null,3,null,3]);
 
         expect(isSymmetricIterative(tree)).toEqual(false);
+    })
+});
+
+describe('isSymmetric (recursive)', () => {
+    it('should return false when tree is null', () => {
+        const tree = setupTree([]);
+        
+        expect(isSymmetricRecursive(null)).toEqual(true);
+    });
+    it('should return true when tree is one level', () => {
+        const tree = setupTree([0]);
+
+        expect(isSymmetricRecursive(tree)).toEqual(true);
+    });
+    it('should return true for symmetric leetcode sample', () => {
+        const tree = setupTree([1,2,2,3,4,4,3]);
+
+        expect(isSymmetricRecursive(tree)).toEqual(true);
+    });
+    it('should return false for asymmetric leetcode sample', () => {
+        const tree = setupTree([1,2,2,null,3,null,3]);
+
+        expect(isSymmetricRecursive(tree)).toEqual(false);
     })
 });
