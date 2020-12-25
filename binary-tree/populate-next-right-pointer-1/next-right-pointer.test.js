8f2e19c167e9f0edef99ff48de7d8099ee0346ae
mcphilip <mcclellandphilip@yahoo.com>
2020-12-25 16:16:51 -0600

    test cases used to figure out solution

---
+++ binary-tree/populate-next-right-pointer-1/next-right-pointer.test.js
@@ -0,0 +1,27 @@
+const { connect, setupTree, treeToArrayWithNextPointer } = require('./next-right-pointer');
+
+describe('treeToArrayWithNextPointer', () => {
+    it('should return correct array for leetcode example tree', () => {
+        const tree = setupTree([1,2,3,4,5,6,7]);
+
+        // manually preform linking that will ultimately be done in connect()
+        tree.left.next = tree.right;
+        tree.left.left.next = tree.left.right;
+        tree.left.right.next = tree.right.left;
+        tree.right.left.next = tree.right.right;
+
+        expect(treeToArrayWithNextPointer(tree)).toEqual([1,'#',2,3,'#',4,5,6,7,'#']);
+    });
+});
+
+describe('connect', () => {
+    it('should return correct serialized tree for null input', () => {
+        expect(treeToArrayWithNextPointer(connect(null))).toEqual([]);
+    });
+    it('should return correct serialized tree for leetcode example tree', () => {
+        const tree = setupTree([1,2,3,4,5,6,7]);
+        const wiredUpTree = connect(tree);
+
+        expect(treeToArrayWithNextPointer(wiredUpTree)).toEqual([1,'#',2,3,'#',4,5,6,7,'#']);
+    })
+})
